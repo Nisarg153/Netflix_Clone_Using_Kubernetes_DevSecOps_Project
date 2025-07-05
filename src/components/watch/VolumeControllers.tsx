bbf85d3f8ea100fa93f521e76f05905ca3c0c56a
Nisarg153 <nisarg150301@gmail.com>
2025-07-04 21:29:53 -0400

    fixed VolumeController.tsx error in sonarqube

--- src/components/watch/VolumeControllers.tsx
+++ src/components/watch/VolumeControllers.tsx
@@ -1,7 +1,6 @@
 import { Stack } from "@mui/material";
-import Slider from "@mui/material/Slider";
+import Slider, { SliderProps }from "@mui/material/Slider";
 import { styled } from "@mui/material/styles";
-import { SliderProps } from "@mui/material/Slider";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import PlayerControlButton from "./PlayerControlButton";
